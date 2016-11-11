9429385c741a217a48624f9c14611e34c2682677
Nicholas Savini <nicholasmsavini@gmail.com>
2016-11-10 20:51:25 -0500

    Create categories class

--- database_schema.py
+++ database_schema.py
@@ -6,12 +6,24 @@ from sqlalchemy import create_engine
 Base = declarative_base()
 
 class User(Base):
+    """
+    Creates the 'user' table. Name, email, and picture all come from the user's
+    gmail account.
+    """
     __tablename__ = 'user'
 
     id = Column(Integer, primary_key = True)
     name =Column(String(80), nullable = False)
     email = Column(String(250), nullable=False)
     picture = Column(String(250))
+
+
+class Categories(Base):
+    """ Creates 'categories' table, containing all possible item categories """
+    __tablename__ = 'categories'
+
+    id = Column(Integer, primary_key = True)
+    category = Column(String(80))
 
 engine = create_engine('sqlite:///catalog.db')
 
