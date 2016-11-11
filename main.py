5794f84947174424a2a75adc0acdfbf03dcc61cd
Nicholas Savini <nicholasmsavini@gmail.com>
2016-11-10 22:13:01 -0500

    Create main.py file for catalog app

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request, redirect
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_schema import Base, User, Category, Item
+
+app = Flask(__name__)
+
+# Connect to Database and create database session
+engine = create_engine('sqlite:///catalog.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='0.0.0.0', port=8000)
